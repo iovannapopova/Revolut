376f51377af13a16d4569879e61056ea294902b2
Iovanna Popova <iovanna@Andreys-MacBook-Air.local>
2017-07-19 11:34:30 +0100

    Initial Commit

---
+++ Revolut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Revolut
+//
+//  Created by Iovanna Popova on 19/07/2017.
+//  Copyright © 2017 IP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
